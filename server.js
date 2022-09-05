8e7282248c65148f2f417f8e73bd53a24b048709
Yair Nava <iryanavahe@ittepic.edu.mx>
2022-09-05 12:12:03 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+var players = {};
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  };
+  console.log(players);
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+  // when a player disconnects, remove them from our players object
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    socket.broadcast.emit('removePlayer', socket.id);
+    socket.disconnect();
+    console.log(players);
+  });
+});
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
