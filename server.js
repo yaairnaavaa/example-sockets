4fac35da7b0e5ddf3bc0b0951f40b9a33b29f5b0
Yair Nava <iryanavahe@ittepic.edu.mx>
2022-09-05 18:00:06 -0600

    add player movement and star collected events methods in sockets

--- server.js
+++ server.js
@@ -38,6 +38,27 @@ io.on('connection', function (socket) {
     socket.disconnect();
     console.log(players);
   });
+
+  socket.on('playerMovement', function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
+
+  socket.on('starCollected', function () {
+    if (players[socket.id].team === 'red') {
+      scores.red += 10;
+    } else {
+      scores.blue += 10;
+    }
+    star.x = Math.floor(Math.random() * 700) + 50;
+    star.y = Math.floor(Math.random() * 500) + 50;
+    io.emit('starLocation', star);
+    io.emit('scoreUpdate', scores);
+  });
+  
 });
 
 const PORT = process.env.PORT || 8082;
